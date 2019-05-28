15c4399a77a76a94e6c93719afc7d96224b69041
Mohammad Hossein Balkhani <M.balkhani@carpino.ir>
2019-05-28 12:32:41 +0430

    *Fixing Capitals

--- gootloader.go
+++ gootloader.go
@@ -8,28 +8,28 @@ import "strconv"
 
 //Bootloader data structure
 type Bootloader struct {
-	name         string
-	arch         int // Architecture 32 or 64
-	modeBit      int // Workspace mode in bits 16 or 32
-	instructions []string
-	loaddest     int
+	Name         string
+	Arch         int // Architecture 32 or 64
+	ModeBit      int // Workspace mode in bits 16 or 32
+	Instructions []string
+	Loaddest     int
 }
 
 func (r *Bootloader) getName(title string) int {
-	println(title + r.name)
+	println(title + r.Name)
 	return 1
 }
 func (r *Bootloader) addInstruction(instruction string) int {
 
-	r.instructions = append(r.instructions, instruction)
+	r.Instructions = append(r.Instructions, instruction)
 	return 1
 
 }
 func (r *Bootloader) create() string {
 
-	fmt.Println("org " + strconv.Itoa(r.loaddest))
-	r.instructions = append([]string{"org " + strconv.Itoa(r.loaddest) + "\n"}, r.instructions...)
-	r.instructions = append([]string{"bits " + strconv.Itoa(r.modeBit) + "\n"}, r.instructions...)
+	fmt.Println("org " + strconv.Itoa(r.Loaddest))
+	r.Instructions = append([]string{"org " + strconv.Itoa(r.Loaddest) + "\n"}, r.Instructions...)
+	r.Instructions = append([]string{"bits " + strconv.Itoa(r.ModeBit) + "\n"}, r.Instructions...)
 
 	r.addInstruction("halt: hlt\n")
 
@@ -38,7 +38,7 @@ func (r *Bootloader) create() string {
 
 	cInst := ""
 
-	for _, element := range r.instructions {
+	for _, element := range r.Instructions {
 
 		cInst += element
 
